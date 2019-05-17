107c577c6d53e62aaf6d923f6698ca998066e3ed
Aidan Rudkovskyi <rrubyist@gmail.com>
2019-05-17 23:20:10 +0200

    fix spec it description

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,7 +1,7 @@
 describe Album do
   subject { described_class.create(name: "Hits of 70s'") }
   context 'when it adds new tag' do
-    it '#tags returns array of tags' do
+    it '#tags return indexed array' do
       subject.tags.add(name: 'impressive')
       subject.tags.add(name: 'charming')
       expect(subject.tags.map(&:name)).to include('impressive', 'charming')
