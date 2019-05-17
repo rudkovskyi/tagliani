60a803f80da6703e0452a82c4f95757bb747de0b
Aidan Rudkovskyi <rrubyist@gmail.com>
2019-05-17 23:17:45 +0200

    Fix indent

--- lib/gossips/configuration/tag.rb
+++ lib/gossips/configuration/tag.rb
@@ -1,9 +1,9 @@
 module Gossips
   class Configuration
     class Tag < Struct.new(:types)
-			def initialize
-				self.types = %i[default inheritable]
-			end
-		end
-	end
+      def initialize
+        self.types = %i[default inheritable]
+      end
+    end
+  end
 end
